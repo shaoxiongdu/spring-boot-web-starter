f5368d7763f248524fad378d1e0814a84ee047da
杜少雄 <dushaoxiong@lixiang.com>
2023-06-14 16:41:10 +0800

    部署脚本
    Signed-off-by: 杜少雄 <dushaoxiong@lixiang.com>

--- 部署.sh
+++ 部署.sh
@@ -1,10 +1,12 @@
+appName="spring-boot-web-starter-0.0.1-SNAPSHOT.jar"
+
 # 编译
 mvn clean
 mvn package
 
 # 上传文件  确保已经配置了SSH公钥身份验证
 
-scp './target/spring-boot-web-starter-0.0.1-SNAPSHOT.jar' root@114.115.209.223:'/root/workspace/spring-boot-web-starter/'
+scp './target/'$appName root@114.115.209.223:'/root/SpringBootJar/'
 
 # 执行
 ssh root@114.115.209.223 << EOF
@@ -12,5 +14,5 @@ ssh root@114.115.209.223 << EOF
 sudo fuser -k 80/tcp
 
 # 后台启动上传的 JAR 文件
-nohup java -jar /root/workspace/spring-boot-web-starter/spring-boot-web-starter-0.0.1-SNAPSHOT.jar --spring.profiles.active=HWCloud > /dev/null 2>&1 &
+nohup java -jar /root/SpringBootJar/$appName --spring.profiles.active=HWCloud > /dev/null 2>&1 &
 EOF
